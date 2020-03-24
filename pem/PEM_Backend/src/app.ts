c2a69b7d473204d5faf001165f2be103e194da38
Dylan Werbrouck <dylan.werbrouck@hotmail.com>
2020-03-24 15:44:21 +0100

    added user model

--- pem/PEM_Backend/src/app.ts
+++ pem/PEM_Backend/src/app.ts
@@ -1,12 +1,42 @@
-import express from "express";
+/**
+ * Required External Modules
+ */
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
+/**
+ * App Variables
+ */
 const app = express();
+const port = process.env.PORT || 5000; // TODO: get port from .env file
+const uri = 'mongodb+srv://Dylan:Passw0rd@cluster0-8qfko.gcp.mongodb.net/test?retryWrites=true&w=majority';
+/**
+ *  App Configuration
+ */
 
-app.get("/", (req, res) => {
-	res.send("");
-})
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB connected');
+});
 
-// TODO: get port from .env file
-const port = 5000;
+/**
+ * Routes Definitions
+ */
 
-app.listen(port);
+app.get('/', (req, res) => {
+	res.send('hello world');
+  });
+
+/**
+ * Server Activation
+ */
+app.listen(port, () => {
+  console.log(`server running at ${port}`);
+});
---
+++ pem/PEM_Backend/src/user.model.ts
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose';
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 2
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
