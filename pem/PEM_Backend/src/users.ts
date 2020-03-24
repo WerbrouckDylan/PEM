35f8dd32fae202e8a1dd22005405391995360438
unknown <louisboucquet@gmail.com>
2020-03-24 17:23:23 +0100

    bug fix

--- pem/PEM_Backend/src/users.ts
+++ pem/PEM_Backend/src/users.ts
@@ -7,7 +7,7 @@ router.route('/').get((req, res) => {
   //res.send('hello users');
   User.find()
     .then(users => res.json(users))
-    .catch(err => res.status(400).json('Error: ' + err));
+    .catch(err => res.status(400).send('Error: ' + err));
 });
 
 router.post('/add', (req, res) => {
